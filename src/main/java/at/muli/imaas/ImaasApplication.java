dddcce0534e9252e5c070187ef7c252149481b1a
mulithemuli <phili@centauri.chef.at>
2018-07-16 23:30:44 +0200

    initial import

---
+++ src/main/java/at/muli/imaas/ImaasApplication.java
@@ -0,0 +1,12 @@
+package at.muli.imaas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImaasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImaasApplication.class, args);
+	}
+}
